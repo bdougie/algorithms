48994090040e26fe18edf87d768594f1522b7649
Brian Douglas <ilikerobot@gmail.com>
2015-10-17 06:48:08 -0700

    added slower 2nd example

--- sum_of_arrays.rb
+++ sum_of_arrays.rb
@@ -1,10 +1,22 @@
 require 'benchmark'
 
 def sum_fast(array)
+  # speed in real time (0.000008)
   sum = array.inject(0, &:+)
   return true if sum == 0
   return false
 end
 
-print Benchmark.measure { sum_fast([1, 1, -2]) }
+#
+# def sum_fast(array)
+#   # speed in real time (0.000005)
+#   sum = 0
+#   array.each do |i|
+#     sum += i
+#   end
+#   return true if sum == 0
+#   return false
+# end
+
 # puts sum_fast([1, 1, -2])
+print Benchmark.measure { sum_fast([1, 1, -2]) }
