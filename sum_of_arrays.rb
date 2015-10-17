7d334beca60eaca6b5f329691d19d7a17d746157
Brian Douglas <ilikerobot@gmail.com>
2015-10-17 07:10:32 -0700

    shaves off .001 milisecond

--- sum_of_arrays.rb
+++ sum_of_arrays.rb
@@ -1,21 +1,23 @@
 require 'benchmark'
 
+# slower example
+#
 # def sum_fast(array)
 #   # speed in real time (0.000008)
 #   sum = array.inject(0, &:+)
-#   return true if sum == 0
-#   return false
+#   return sum == 0
 # end
 
+# faster example
+#
 def sum_fast(array)
-  # speed in real time (0.000005)
+  # speed in real time (0.000004)
   sum = 0
   array.each do |i|
     sum += i
   end
 
-  return true if sum == 0
-  return false
+  return sum == 0
 end
 
 # puts sum_fast([1, 1, -2])
